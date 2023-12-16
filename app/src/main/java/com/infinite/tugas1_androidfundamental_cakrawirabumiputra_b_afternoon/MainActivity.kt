db8ef51cd690d70d0fbf872da1a6e6a265805095
cakrawrz_ <cakradwi1@gmail.com>
2023-12-16 22:41:35 +0700

    Initial commit

---
+++ app/src/main/java/com/infinite/tugas1_androidfundamental_cakrawirabumiputra_b_afternoon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.infinite.tugas1_androidfundamental_cakrawirabumiputra_b_afternoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
